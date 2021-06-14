5ae83b6777c9111e90eaac21de1b1ca1c61fa0f0
TomDorani <62829614+TomDorani@users.noreply.github.com>
2021-06-14 23:21:07 +0300

    weshallbegin

--- femub/src/App.js
+++ femub/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Button } from "@material-ui/core";
+import Amplify, { Analytics } from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+Analytics.autoTrack("pageView", {
+	enable: true,
+	type: "SPA",
+});
+
+Analytics.autoTrack("event", {
+	enable: true,
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	Analytics.record({ name: "albumVisit" });
+	return (
+		<div className="App">
+			<AmplifySignOut />
+			<header>
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>We now have Auth!</h1>
+			</header>
+			<Button
+				color="primary"
+				onClick={() => {
+					Analytics.record({ name: "albumVisitAfter" });
+					alert("Hey you doin' it Wrong");
+				}}
+			>
+				Hello World
+			</Button>
+			;
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
